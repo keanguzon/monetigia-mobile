b1f8a52d46de6a33461155f12bcc95dd9814637f
User <user@example.com>
2026-07-06 15:55:13 +0800

    fix: change oauth redirect path to /login to fix unmatched route

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -15,7 +15,7 @@ export default function LoginScreen() {
 
   const redirectTo = makeRedirectUri({
     scheme: "monetigia",
-    path: "auth/callback"
+    path: "login"
   });
 
   const handleTokens = async (url: string) => {
